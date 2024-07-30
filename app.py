50fe9c8b176f3835406b0c7d933136585d86e9dc
Adamu Jighjigh <0j144k@gmail.com>
2024-07-30 11:13:11 +0100

    Add create book functionality

--- app.py
+++ app.py
@@ -9,4 +9,29 @@ db_config = {
     'host': '127.0.0.1',
     'database': 'library_db'
 }
+def get_db_connection():
+    conn = mysql.connector.connect(**db_config)
+    return conn
 
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM books')
+    books = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', books=books)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        year = request.form['year']
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO books (title, author, year) VALUES (%s, %s, %s)', (title, author, year))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('create.html')
